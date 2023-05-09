687b49998592770879d5b508fed8b1c38ea03a96
“yanliangming” <yanliangming@oasgames.com>
2023-05-09 19:03:43 +0800

    token增加密码修改校验

--- backend/api/v1/login.py
+++ backend/api/v1/login.py
@@ -28,7 +28,7 @@ def login_access_token(
     access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     return {
         "access_token": security.create_access_token(
-            user.id, expires_delta=access_token_expires
+            user.hashed_password, user.id, expires_delta=access_token_expires,
         ),
         "token_type": "bearer",
     }
