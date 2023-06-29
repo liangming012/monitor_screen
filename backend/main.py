384a016781512c82b4101dfc8de4903d36eac252
“yanliangming” <yanliangming@oasgames.com>
2023-06-29 19:36:37 +0800

    增加健康检查

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,23 @@
+import os
+
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 from fastapi import APIRouter
 from starlette.middleware.httpsredirect import HTTPSRedirectMiddleware
 
-from api.v1 import login, users, projects, records, screens, shows, notices
+from api.v1 import login, users, projects, records, screens, shows, notices, healthy
 from core.config import settings
 # 不要使用以下代码自动创建数据库表，请使用 alembic的自动生成数据库迁移和迁移升级到最新版本命令
 # from api.deps import engine
 # from models.base import Base
 # Base.metadata.create_all(bind=engine)
-app = FastAPI(
-    title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json"
-)
+env = os.getenv('INSTALL_DEV')
+if env != 'production':
+    app = FastAPI(title=settings.PROJECT_NAME, docs_url=None, redoc_url=None, openapi_url=None)
+else:
+    app = FastAPI(
+        title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json"
+    )
 # 支持https请求
 if settings.ENABLE_HTTPS:
     app.add_middleware(HTTPSRedirectMiddleware)
@@ -33,4 +39,5 @@ api_router.include_router(records.router, prefix="/records", tags=["records"])
 api_router.include_router(screens.router, prefix="/screens", tags=["screens"])
 api_router.include_router(shows.router, prefix="/shows", tags=["shows"])
 api_router.include_router(notices.router, prefix="/notices", tags=["notices"])
+api_router.include_router(healthy.router, prefix="/healthy", tags=["healthy"])
 app.include_router(api_router, prefix=settings.API_V1_STR)
