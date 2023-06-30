51666e94f17202d9ee5de191bbe58977e3b9f940
“yanliangming” <yanliangming@oasgames.com>
2023-06-30 16:13:36 +0800

    fix bug

--- backend/run_uvicorn.py
+++ backend/run_uvicorn.py
@@ -5,6 +5,6 @@ from main import app
 
 if __name__ == "__main__":
     if settings.ENABLE_HTTPS:
-        uvicorn.run(app, ssl_keyfile='oasgames.com.key', ssl_certfile='oasgames.com.crt')
+        uvicorn.run(app, host='0.0.0.0', ssl_keyfile='oasgames.com.key', ssl_certfile='oasgames.com.crt')
     else:
-        uvicorn.run(app)
+        uvicorn.run(app, host='0.0.0.0')
